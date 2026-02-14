558dd1b0a5429d9862b27741ab886ec028beb6b9
von9880 <tony.auron.brown@gmail.com>
2026-02-14 15:22:18 -0500

    Update Task.js

--- Classes/Task.js
+++ Classes/Task.js
@@ -1,23 +1,47 @@
 const TASK_STATES = {
     TODO: "Todo",
     ARCHIVED: "Archived",
-    DONE: "Done"
+    DONE: "Done",
+    DELETED: "Deleted"
 }
 
-
-const DEFAULT_TASK_NAME =   "New Task";
+//defaults for instance vars
+const DEFAULT_TASK_NAME   = "New Task";
 const DEFAULT_DESCRIPTION = "";
-const DEFAULT_STATUS =      TASK_STATES.TODO;
-const DEFAULT_POSITION =    0;
-const DEFAULT_FINISHED =    false;
+const DEFAULT_STATUS      = TASK_STATES.TODO;
+const DEFAULT_POSITION    = 0;
+const DEFAULT_FINISHED    = false;
+
+//confirm button settings (offsets so far)
+const CONFIRM_X_OFFSET    = 10
+const CONFIRM_Y_OFFSET    = 10
+
+//cancel button settings (offsets so far)
+const CANCEL_X_OFFSET     = 350
+const CANCEL_Y_OFFSET     = 10
+
+//text settings
+const TEXT_X_OFFSET       = 190
+const TEXT_X_PADDING      = 0   //not used yet
+const TEXT_Y_OFFSET       = 0   //not used yet
+const TEXT_Y_PADDING      = 30
 
 class Task {
     constructor(name, desc, status, position) { 
-        this.name =        name     || DEFAULT_TASK_NAME;
+        this.name        = name     || DEFAULT_TASK_NAME;
         this.description = desc     || DEFAULT_DESCRIPTION;
         this.status =      status   || DEFAULT_STATUS;
         this.position =    position || DEFAULT_POSITION;
         this.finished =                DEFAULT_FINISHED;
+
+        this.markTaskDoneButton = createButton(`Mark Done`);
+        this.markTaskDoneButton.hide();
+        this.markTaskDoneButton.mousePressed(() => this.buttonPressedMarkDone());
+        
+        this.deleteTaskButton = createButton(`Delete Task`);
+        this.deleteTaskButton.hide();
+        this.deleteTaskButton.mousePressed(() => this.buttonPressedDelete());
+
     }
 
     //getters and setters
@@ -27,11 +51,11 @@ class Task {
     getPosition() { return this.position}
     isFinished()  { return this.finished }
 
-    setName(newName)       { this.name =        newName   || DEFAULT_TASK_NAME }
+    setName(newName)       { this.name        = newName   || DEFAULT_TASK_NAME }
     setDesc(newDesc)       { this.description = newDesc   || DEFAULT_DESCRIPTION }
-    setStatus(newStatus)   { this.status =      newStatus || DEFAULT_STATUS }
-    setPosition(newPos)    { this.position =    newPos    || DEFAULT_POSITION }
-    setFinished(condition) { this.finished =    condition || DEFAULT_FINISHED }
+    setStatus(newStatus)   { this.status      = newStatus || DEFAULT_STATUS }
+    setPosition(newPos)    { this.position    = newPos    || DEFAULT_POSITION }
+    setFinished(condition) { this.finished    = condition || DEFAULT_FINISHED }
 
     //im not saying that this method should be in the list but it should definitely be in the list
     setArchived() {
@@ -44,6 +68,13 @@ class Task {
         this.setFinished(true);
     }
 
+    //get the list class to remove this or something
+    delete() {
+        this.setName("Deleted Task")
+        this.setDesc("")
+        this.setStatus(TASK_STATES.DELETED)
+    }
+
     //basically slides the position of the task up or down 1 spot (world record for fastest annihilation of the webapp)
     slidePosition(direction) {
         if (direction == 0) { //do not.
@@ -52,12 +83,70 @@ class Task {
         let normDirection = direction / Math.abs(direction); //wizard spell to normalize the direction to either 1 or -1 (in theory)
         this.setPosition(normDirection);
     }
+    
 
     //methods
     toString() {
-        return `Name: ${this.name}\n Description: ${this.description}\n Status: ${this.status}\n Position: ${this.position}\n Is Finished: ${this.finished}`;
+        let output = ""
+
+        output += `Name: ${this.name}\n`
+        output += `Description: ${this.description}\n`
+        output += `Status: ${this.status}\n`
+        output += `Position: ${this.position}\n`
+        output += `Is Finished: ${this.finished}`
+
+        return output;
     }
 
+    deleteTaskButtons(){
+        this.markTaskDoneButton.remove();
+        this.deleteTaskButton.remove();
+    }
+
+    buttonPressedMarkDone(){
+        this.setCompleted();
+        refresh();
+    }
+
+    buttonPressedDelete(){
+        let list = this.getListTask()
+        this.deleteTaskButtons()
+        list.removeTask(this);
+        refresh();
+    }
+
+    //gets the list that the task is in
+    getListTask(){
+        for(let list of listArray){
+            let storage = list.getStorage();
+            if(storage.findIndex(t => t.name === this.name) != -1){
+                return list
+            }
+        }
+    }
+
+    show(x, y) {
+
+        // main box
+        rect(x, y, 380, 120, 10);
+
+        // sets pos of buttons        
+        this.markTaskDoneButton.position(x + 10, y+10);
+        this.deleteTaskButton.position(x + 285, y+10);
+
+        //shows buttons
+        this.markTaskDoneButton.show();
+        this.deleteTaskButton.show();
+
+        // text slop
+        textAlign(CENTER, CENTER);
+        fill(0);
+        text(this.name, x + TEXT_X_OFFSET, y + TEXT_Y_PADDING);
+        text(this.description, x + TEXT_X_OFFSET, y + TEXT_Y_PADDING * 2);
+        fill(255, 0, 0);
+        text(this.status, x + TEXT_X_OFFSET, y + TEXT_Y_PADDING * 3);
+        fill(255);
+    }
 
     static fromJSON(data) {
         return new Task(
@@ -69,10 +158,3 @@ class Task {
         );
     }
 }
-    
-
-
-
-
-    
-
